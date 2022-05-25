0ed118633336e3efd98e31aa29f9dcce3e446cc2
Shayan <shayansadeghi178@gmail.com>
2022-05-25 20:14:00 +0430

    add new echo image view

--- echo/views.py
+++ echo/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'echo/index.html')
+
+
+def image(request):
+    return render(request, 'echo/echo_image.html')

e0a9ac563b66473da23e86979ec244441b9ecb1a
Shayan <shayansadeghi178@gmail.com>
2022-05-25 20:13:49 +0430

    add new echo image url

--- echo/urls.py
+++ echo/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'echo'
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('image',views.image,name='image'),
 ]

44a751bc385c668d883bd21de7e454a61afada79
Shayan <shayansadeghi178@gmail.com>
2022-05-27 19:08:06 +0430

    add http protocol to the app

--- echo_websocket/routing.py
+++ echo_websocket/routing.py
@@ -1,10 +1,16 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+
 from echo import routing as echo_websocket_routing
+from chat import routing as chat_routing
+
 application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            echo_websocket_routing.websocket_urlpatterns
+            # echo_websocket_routing.websocket_urlpatterns,
+            chat_routing.websocket_urlpatterns
         )
     )
 })

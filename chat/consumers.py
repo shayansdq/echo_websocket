151cf74db6759a2cb8aec45fd5ff091f06308e3e
Shayan <shayansadeghi178@gmail.com>
2022-06-02 13:14:11 +0430

    start write customizable consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,39 +1,58 @@
 import json
-
-from channels.generic.websocket import WebsocketConsumer
+from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.username = self.scope['url_route']['kwargs']['username']
         self.group_name = f"chat_{self.username}"
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
 
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         if text_data:
             text_data_json = json.loads(text_data)
             username = text_data_json['receiver']
             user_group_name = f"chat_{username}"
 
-            async_to_sync(self.channel_layer.group_send)(
+            await self.channel_layer.group_send(
                 user_group_name,
                 {
                     'type': 'chat_message',
                     'message': text_data
-                },
-            )
+                })
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
-        self.send(text_data=message)
+        await self.send(text_data=message)
+
+
+class ChatConsumer2(AsyncConsumer):
+    async def websocket_connect(self, event):
+        self.user_id = self.scope['url_route']['kwargs']['username']
+        self.group_name = f"chat_{self.user_id}"
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.send({
+            'type': 'websocket.accept',  # websocket_accept
+        })
+
+    async def websocket_disconnect(self, event):
+        pass
+
+    async def websocket_receive(self, event):
+        pass

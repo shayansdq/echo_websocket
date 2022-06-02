a5659aa85da16bdf0f52e3151031a1430c2d5dc1
Shayan <shayansadeghi178@gmail.com>
2022-06-02 18:57:27 +0430

    write new message func

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 
 # Create your views here.
 
 def join_chat(request, username):
     return render(request, 'chat/join_chat.html', context={'username_json': mark_safe(json.dumps(username))})
+
+
+def new_message(request, username):
+    receiver = request.GET.get('receiver')
+    text = request.GET.get('text')
+    channel_layer = get_channel_layer()
+    group_name = f"chat_{receiver}"
+    async_to_sync(channel_layer.group_send)(
+        group_name,
+        {
+            'type': 'chat_message',
+            'message': json.dumps({'sender': username, 'receiver': receiver, 'text': text})
+        }
+    )
+    return HttpResponse('Message sent')

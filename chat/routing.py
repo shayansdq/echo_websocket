e9e42c803096532a4f1bb9939a12bca256b22b8d
Shayan <shayansadeghi178@gmail.com>
2022-06-02 18:54:49 +0430

    create new routing for new consumer

--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import consumers
 websocket_urlpatterns = [
-    path('ws/chat/<str:username>/',consumers.ChatConsumer.as_asgi())
+    path('ws/chat/<str:username>/',consumers.ChatConsumer.as_asgi()),
+    path('ws/chat2/<str:username>/',consumers.ChatConsumer2.as_asgi()),
 ]

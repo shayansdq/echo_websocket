7358ed1960f63c42294f0dc15946b21b43741f35
Shayan <shayansadeghi178@gmail.com>
2022-06-02 18:57:10 +0430

    add new message url to send message

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
 app_name = 'chat'
 urlpatterns = [
-    path('chat/<str:username>/',views.join_chat,name='join_chat'),
+    path('chat/<str:username>/', views.join_chat, name='join_chat'),
+    path('chat/new/<str:username>/', views.new_message, name='new_message'),
 ]
